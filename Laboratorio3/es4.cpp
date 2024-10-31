c59a4f2275e3c1765dbc344ec8f7f311198c76fe
Mattia-Zanini <mattzanini2@gmail.com>
2024-10-31 10:54:30 +0100

    scritte le funzioni membro della classe MyVector e definito il distruttore

--- Laboratorio3/es4.cpp
+++ Laboratorio3/es4.cpp
@@ -1,9 +1,33 @@
 class MyVector {
   int size;
-  double *array = nullptr;
+  double *array;
 
 public:
-  MyVector() { array = new double[1]; }
+  MyVector();
+  MyVector(int n);
+  void safe_set(int pos, double value);
+  double safe_get(int pos);
+  double operator[](const int pos);
+  ~MyVector();
 };
 
-int main() { return 0; }
+MyVector::MyVector() {
+  size = 1;
+  array = new double[size];
+}
+MyVector::MyVector(int n) {
+  size = n;
+  array = new double[n];
+}
+
+void MyVector::safe_set(int pos, double value) {}
+double MyVector::safe_get(int pos) { return 0; }
+double MyVector::operator[](const int pos) { return 0; }
+MyVector::~MyVector() {
+  delete[] array;
+}
+
+int main() {
+  MyVector v = MyVector();
+  return 0;
+}
