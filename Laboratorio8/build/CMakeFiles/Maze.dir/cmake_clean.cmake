992acda7b24b745bfe94b7651ed09a28271f30e0
Mattia-Zanini <mattzanini2@gmail.com>
2024-12-11 16:02:40 +0100

    aggiunto il cmake list per utilizzare cmake

---
+++ Laboratorio8/build/CMakeFiles/Maze.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Maze.dir/src/Maze.cpp.o"
+  "CMakeFiles/Maze.dir/src/Maze.cpp.o.d"
+  "libMaze.a"
+  "libMaze.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Maze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
