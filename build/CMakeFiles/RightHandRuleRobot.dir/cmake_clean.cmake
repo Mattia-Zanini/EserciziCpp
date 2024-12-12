d915deed566b978db587a0ed278e996a5a29be16
Mattia-Zanini <mattzanini2@gmail.com>
2024-12-12 10:18:21 +0100

    cmake mi ha scassato le balle per sti file

---
+++ build/CMakeFiles/RightHandRuleRobot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RightHandRuleRobot.dir/src/RightHandRuleRobot.cpp.o"
+  "CMakeFiles/RightHandRuleRobot.dir/src/RightHandRuleRobot.cpp.o.d"
+  "libRightHandRuleRobot.a"
+  "libRightHandRuleRobot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RightHandRuleRobot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
